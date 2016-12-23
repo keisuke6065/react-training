d18c3d3c58f7b6f03a5755f6fc9647df0e1efe68
中村 恵介 <keisuke.nakamura@bizreach.co.jp>
2016-12-23 19:26:24 +0900

    css test

---
+++ src/componets/body.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './body.css';
+
+class Body extends Component {
+    render() {
+        return (
+            <div className="body">
+                <h2>hello world</h2>
+            </div>
+        );
+    }
+}
+
+export default Body;
---
+++ src/componets/nav.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './nav.css'
+
+class Nav extends Component {
+    render() {
+        return (
+            <div className="nav">
+                <h2>test</h2>
+            </div>
+        );
+    }
+}
+
+export default Nav;
